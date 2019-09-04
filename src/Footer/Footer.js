7402790a80cce485bef9104a8796ddf513f51210
Łukasz Szlaski <lucashlszl@gmail.com>
2019-09-04 09:36:36 +0200

    awesome

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Footer.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCircel} from '@fortawesome/free-regular-svg-icons';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -17,7 +20,7 @@ render () {
     <div className="container footer-container">
         <h2>Nazwa firmy - wszelkie prawa zastrzeżone, 2019</h2>
         <div className="social-container">
-        {/* <FontAwesomeIcon icon = {coffee}/> */}
+        <FontAwesomeIcon icon="coffee" />
             <a className="fab fa-instagram fa-2x" href="https://www.instagram.com/" target="_blank"></a>
             <a className="fab fa-facebook-square fa-2x" href="https://www.facebook.com/" target="_blank"></a>
         </div>
